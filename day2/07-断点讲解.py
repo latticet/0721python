0a89f63b3c6693f097c285a74ef703d1bb40377a
zhangyaowen <zhangyaowenjy@163.com>
2022-09-15 14:35:01 +0800

    断点讲解

---
+++ day2/07-断点讲解.py
@@ -0,0 +1,25 @@
+"""
+pycharm断点使用
+"""
+score = int(input('分数：'))  # 80
+
+if score >= 90:
+    print('优秀')
+elif score >= 80:
+    print('良好')
+elif score >= 60:
+    print('及格')
+else:
+    print('不及格')
+
+
+print('end')
+
+
+# debug
+# 说明：代码调试
+# 1. 打断点 （程序运行起来后，暂停位置）
+# 2. 以debug方式运行代码
+# 3. 在debug下手动执行代码
+
+
