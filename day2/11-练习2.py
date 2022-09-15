1a1bb369b4084296b39d5fb5bc481a194245475b
zhangyaowen <zhangyaowenjy@163.com>
2022-09-15 16:05:44 +0800

    练习2

--- day2/11-练习2.py
+++ day2/11-练习2.py
@@ -6,3 +6,27 @@
  方法：讲课
 班主任类（员工类）
 """
+
+
+# 员工类
+class Staff:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def run(self):
+        print('跑步')
+
+    def eat(self):
+        print('吃饭')
+
+
+# 讲师类
+class Teacher(Staff):
+    def teach(self):
+        print('讲课')
+
+
+# 班主任类
+class HeadMaster(Staff):
+    pass
