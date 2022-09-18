9104518c420c86cc29c32d313e8dff9a70ea9aac
zhangyaowen <zhangyaowenjy@163.com>
2022-09-18 15:18:40 +0800

    模块用途

---
+++ day4/06-模块其他/03-模块用途.py
@@ -0,0 +1,3 @@
+import module2
+
+# 预见
--- day4/06-模块其他/module2.py
+++ day4/06-模块其他/module2.py
@@ -1,3 +1,15 @@
 name = 'module2'
 
-print('module2被执行了')
+
+def fn2():
+    print('fn2执行了')
+
+
+# print('==' * 20)
+# 模块方式执行： 模块名
+# 脚本方式执行： __main__
+# print(__name__)
+
+# 如果__name__值是__main__， 当前文件就是以脚本方式执行
+if __name__ == '__main__':
+    fn2()
