44ddd901034f8a7409a4de1715ce064bebb31390
zhangyaowen <zhangyaowenjy@163.com>
2022-09-18 10:38:35 +0800

    自定义异常

---
+++ day4/02-自定义异常.py
@@ -0,0 +1,27 @@
+"""
+class NameError(Exception):
+    def __str__(self):
+        return '异常描述信息'
+"""
+
+
+# 自定义异常类
+# 需要继承自Exception
+class LenError(Exception):
+    def __str__(self):
+        return '长度不符合规范'
+
+
+# 自定义异常抛出
+# 语法：raise 异常对象
+
+# 需求：验证用户名长度3-8位，不是就抛出异常。
+# 接收用户名
+username = input('用户名：')
+try:
+    if len(username) < 3 or len(username) > 8:
+        raise LenError()
+    else:
+        print('用户名验证通过')
+except Exception as e:
+    print(e)
