befe3090da0826e7d29965b268ea764f6b5b9983
zhangyaowen <zhangyaowenjy@163.com>
2022-09-18 15:58:08 +0800

    导包查找路径

---
+++ day4/07-导包路径/02-模块查找路径.py
@@ -0,0 +1,16 @@
+# 随机模块， 封装了很多随机数的函数
+# 系统标准包模块
+import sys
+from pprint import pprint
+
+import random
+import time
+
+# print(time.name)
+
+# print(random.name)
+
+# for i in range(3):
+#     print(random.randint(1, 10))
+
+pprint(sys.path)
