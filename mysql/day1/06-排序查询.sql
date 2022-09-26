a63367ae3dc2d708f6611c0cb87c33863f118a20
latticet <latticet@163.com>
2022-09-26 12:00:07 +0800

    准备数据文件改名

--- mysql/day1/06-排序查询.sql
+++ mysql/day1/06-排序查询.sql
@@ -1,3 +1,9 @@
+/*
+SELECT 字段列表 FROM 表名 ORDER BY 字段1 [排序方式] , 字段2 [排序方式], 字段N [排序方式];
+● 排序方式
+  ○ ASC : 升序(默认值)
+  ○ DESC: 降序
+*/
 -- sql语句的默认排序 ：主键id升序
 SELECT * FROM student;
 SELECT * FROM student WHERE stu_name in ('张飞', '关羽', '袁术');
