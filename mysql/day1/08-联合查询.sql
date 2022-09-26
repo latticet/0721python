4cddb77fa1d24391a6523242279703b86545399c
latticet <latticet@163.com>
2022-09-26 14:44:53 +0800

    联合查询

---
+++ mysql/day1/08-联合查询.sql
@@ -0,0 +1,33 @@
+/*
+SELECT 字段列表 FROM 表A ... 
+UNION [ ALL ]
+SELECT 字段列表 FROM 表B ....;
+*/
+
+-- 查询年龄大于18和小于18的学生信息
+SELECT * FROM student WHERE stu_age > 18
+UNION ALL
+SELECT * FROM student WHERE stu_age < 18;
+
+-- 查询student表的stu_no和stuname
+-- 查询class表的classname和classroom
+SELECT stu_no, stu_name FROM student
+UNION ALL
+SELECT class_name, class_room FROM class;
+
+-- UNION 单字段去重
+-- 查询id大于等于4的数据和id小于等于3的学生信息
+SELECT stu_age FROM student WHERE stu_id >= 4
+UNION
+SELECT stu_age FROM student WHERE stu_id <= 3;
+
+
+-- UNION ALL 不去重
+-- 查询id大于等于4的数据和id小于等于3的学生信息
+SELECT stu_age FROM student WHERE stu_id >= 4
+UNION All
+SELECT stu_age FROM student WHERE stu_id <= 3;
+
+
+
+
