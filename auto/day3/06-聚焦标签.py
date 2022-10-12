325a0b1fdb2339a7cfaa344ed1a7b9b824d3165f
latticet <latticet@163.com>
2022-10-12 15:09:23 +0800

    selenium操作js

--- auto/day3/06-聚焦标签.py
+++ auto/day3/06-聚焦标签.py
@@ -9,7 +9,7 @@ time.sleep(2)
 
 # 滚动条聚焦到某个标签
 # 定位要聚焦的标签
-target = driver.find_element_by_link_text('外交部：已有1300多名印留学生获得来华签证')
+target = driver.find_element_by_link_text('已有1300多名印留学生获得来华签证')
 print(target.get_attribute('outerHTML'))
 driver.execute_script('arguments[0].scrollIntoView();', target)
 
