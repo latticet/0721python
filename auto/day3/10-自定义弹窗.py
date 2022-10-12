9f0b3bfb36bf804e6741eb82ab111d69b13b33f7
latticet <latticet@163.com>
2022-10-12 18:59:57 +0800

    窗口切换和弹窗处理

--- auto/day3/10-自定义弹窗.py
+++ auto/day3/10-自定义弹窗.py
@@ -12,7 +12,7 @@ time.sleep(2)
 # 测试操作：正常用户的操作
 # close_element = driver.find_element_by_xpath('//*[@id="iframe_company_mini_div"]/h6/span[2]')
 # close_element.click()
-
+# javascript = ECMAScript + Bom + Dom
 js = "div1 = document.getElementById('iframe_company_mini_div');div1.style.display='none'"
 driver.execute_script(js)
 
