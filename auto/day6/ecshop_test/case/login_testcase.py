3241e417a13403df82796536846109efb54a2718
latticet <latticet@163.com>
2022-10-16 18:01:18 +0800

    ecshop_test

--- auto/day6/ecshop_test/case/login_testcase.py
+++ auto/day6/ecshop_test/case/login_testcase.py
@@ -1,10 +1,20 @@
 import unittest
+
+import ddt
+
 from common.base import get_driver
-from page.login_page import LoginPage
 from page.index_page import IndexPage
+from page.login_page import LoginPage
+from common.data_operation import DataOperation
+
+data = DataOperation('user_info.csv')
+user_data = data.get_data_to_dict()
 
 
+@ddt.ddt
 class LoginTestCase(unittest.TestCase):
+    user_data = user_data
+
     @classmethod
     def setUpClass(cls) -> None:
         # 创建浏览器对象，打开浏览器
@@ -14,13 +24,14 @@ class LoginTestCase(unittest.TestCase):
         cls.login = LoginPage(driver)
         cls.index = IndexPage(driver)
 
-    def test_01_login(self):
+    @ddt.data(*user_data)
+    def test_01_login(self, userinfo):
         # TODO 操作流程'
-        expect_username = 'fine1'
+        expect_username = userinfo['username']
         # 输入用户名
         self.login.input_username(expect_username)
         # 输入密 码
-        self.login.input_password('123456')
+        self.login.input_password(userinfo['password'])
         # 点击登 录
         self.login.input_submit()
 
@@ -29,7 +40,11 @@ class LoginTestCase(unittest.TestCase):
         # fine1 和 获取实际结果
         # 定位预期结果
         actual_username = self.index.font_username_text()
-        self.assertEqual(expect_username, actual_username, msg='断言登录成功')
+        self.assertEqual(expect_username, actual_username, msg=userinfo['title'])
+
+    def tearDown(self) -> None:
+        self.index.a_logout()
+        self.index.a_login()
 
     @classmethod
     def tearDownClass(cls) -> None:
--- auto/day6/ecshop_test/page/index_page.py
+++ auto/day6/ecshop_test/page/index_page.py
@@ -7,3 +7,9 @@ from selenium.webdriver.common.by import By
 class IndexPage(Base):
     def font_username_text(self):
         return self.get_text((By.XPATH, '//font[@class="f4_b"]'))
+
+    def a_logout(self):
+        self.click((By.LINK_TEXT, '退出'))
+
+    def a_login(self):
+        self.click((By.PARTIAL_LINK_TEXT, '请登录'))
